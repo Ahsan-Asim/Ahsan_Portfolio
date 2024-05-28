127bfb8a4871a1f973030d63c57c1f3e703e7e0e
Ahsan-Asim <ahsanasim252@gmail.com>
2024-05-28 10:52:50 +0500

    Revert "Change port":title
    This reverts commit 0ee235aca595715fbdcea31e89a8ba879ccec0ed.desc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path'); // Import the path module
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
